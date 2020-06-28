e375d1da9b944e646c219c9bc18fbd84d0441f41
wolfmenwolf <wolfmenwolf@163.com>
2020-06-28 17:43:25 +0800

    'webpack_project_int'

---
+++ frame-project-interview-qh/webpack-demo/build-base-conf/webpack.dev.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const webpackCommonConf = require('./webpack.common');
+const {smart} = require('webpack-merge');
+const {srcPath,distPath} = require('./paths');
+
+module.exports = smart(webpackCommonConf,{
+  mode:'development',
+   module:{
+      rules:[
+          // 直接引入图片 url
+          {
+              test:/\.(png|jpg|jpeg|gif)$/,
+              use:'file-loader'
+          }
+      ]
+  },
+    plugins:[
+        new webpack.DefinePlugin({
+            // window.ENV = 'development'
+            ENV:JSON.stringify('development')
+        })
+    ],
+    devServer:{
+      port:8080,
+        progress:true,    //显示打包进度条
+        contentBase:distPath,//根目录
+        open:true,   //自动打开浏览器
+        compress:true,//启动gzip压缩
+
+        //设置代理
+        proxy:{
+            // 将本地 /api/xxx 代理到 localhost:3000/api/xxx
+            '/api': 'http://localhost:3000',
+            // 将本地 /api2/xxx 代理到 localhost:3000/xxx
+            '/api2': {
+                target: 'http://localhost:3000',
+                pathRewrite: {
+                    '/api2': ''
+                }
+            }
+        }
+    }
+});
